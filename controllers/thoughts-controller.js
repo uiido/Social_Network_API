664be8f5a219e358a9db7671066ec4a6b1315b7a
uiido <uiido@yahoo.com>
2023-01-31 23:36:05 -0500

    Outlined user and thought controllers

---
+++ controllers/thoughts-controller.js
@@ -0,0 +1,19 @@
+const { User, Thought } = require("../models");
+
+module.exports = {
+    // getThoughts
+
+    // getSingleThought
+
+    // createThought
+
+    // deleteThought
+
+    // updateThought
+
+    // addReaction
+
+    // removeReaction
+};
+
+module.exports = thoughtController;
---
+++ controllers/user-controller.js
@@ -0,0 +1,19 @@
+const { User, Thought } = require("../models");
+
+module.exports = {
+    // getUsers
+
+    // getSingleUser
+
+    // createUser
+
+    // deleteUser
+
+    // updateUser
+
+    // addFriend
+
+    // removeFriend
+};
+
+module.exports = userController;
